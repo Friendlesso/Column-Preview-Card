306e4a875d8842dbbe754a72d4d1938135df17bc
Mihailo <mihailoless@gmail.com>
2025-12-09 21:55:03 +0100

    Added hover to buttons

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -6,11 +6,13 @@ type CardProps = {
   color: string,
   textColor: string,
   hovered: string | null;
-  
+  setHovered: React.Dispatch<React.SetStateAction<string | null>>
+
 }
 
-export function Card({ title, description, icon, color, textColor }: CardProps) {
+export function Card({ title, description, icon, color, textColor, hovered, setHovered }: CardProps) {
 
+  const isHovered = hovered === title;
 
   return (
     <article className={`card h-[33vh] sm:h-[60vh] sm:max-w-[30vw] sm:w-84 ${color} flex flex-col px-10 py-10 text-left`}>
@@ -21,7 +23,18 @@ export function Card({ title, description, icon, color, textColor }: CardProps) 
         <h3 className="text-3xl text-gray-100">{title.toUpperCase()}</h3>
         <p className="text-[#ffffffbf]">{description}</p>
       </div>
-      <a href="#" className={` ${textColor} border border-gray-100 w-fit px-8 py-2 rounded-3xl bg-white mt-auto`}>Learn more</a>
+      <a
+        href="#"
+        className={` ${isHovered ? 'border border-gray-100 text-white ' :  `bg-white ${textColor}`} box-border w-fit px-8 py-2 rounded-3xl  mt-auto transition-colors duration-150`}
+        onMouseEnter={() => {
+          setHovered(title);
+        }}
+        onMouseLeave={() => {
+          setHovered(null)
+        }}
+      >
+        Learn more
+      </a>
     </article>
   )
 }
